9242082fd02655769fd1a6c57e5336558b2274a5
frankrs <frankrs3d@gmail.com>
2014-12-29 21:22:30 -0500

    changed balloon prefab added scripts

---
+++ Assets/scripts/BaloonMotor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaloonMotor : MonoBehaviour {
+
+	public BaloonControls controls;
+	public Motor motor;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if(controls.gasOn){
+			rigidbody2D.AddForce(new Vector2(0f,motor.gasPower));
+		}
+	}
+}
+
+
+[System.Serializable]
+public class BaloonControls{
+	public bool gasOn;
+}
+
+[System.Serializable]
+public class Motor{
+	public float gasPower;
+}
---
+++ Assets/scripts/PCControls.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PCControls : MonoBehaviour {
+
+	public BaloonMotor bMotor;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		bMotor.controls.gasOn = Input.GetKey("space");
+	}
+}
