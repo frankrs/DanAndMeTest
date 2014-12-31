66182ce3edd221e68ced8482f61455a4bd956d74
frankrs <frankrs3d@gmail.com>
2014-12-31 00:46:17 -0500

    started making the windzone imported some assets trimmed the grass sptite

---
+++ Assets/scripts/Wind.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Wind : MonoBehaviour {
+
+	public WindZone windZone;
+
+	public ParticleEmiter2D emiter;
+
+	void OnDrawGizmos () {
+		Gizmos.DrawIcon(transform.position,"wind_blowing_cloud.png", true);
+		Gizmos.color = Color.blue;
+		Gizmos.DrawCube(transform.position, new Vector3(.25f, windZone.zoneHight, 0f));
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
+
+
+[System.Serializable]
+public class WindZone{
+	public float zoneHight;
+	public AnimationCurve speedCurve;
+}
+
+[System.Serializable]
+public class ParticleEmiter2D {
+	public GameObject particle;
+}
