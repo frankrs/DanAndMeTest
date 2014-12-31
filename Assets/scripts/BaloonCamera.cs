39ffdeebba5870ea18cfea8481c555feb63720a4
frankrs <frankrs3d@gmail.com>
2014-12-31 15:26:43 -0500

    improved camerascript

--- Assets/scripts/BaloonCamera.cs
+++ Assets/scripts/BaloonCamera.cs
@@ -11,15 +11,18 @@ public class BaloonCamera : MonoBehaviour {
 
 	public float yOffset;
 
+	private float yClamp;
 
 	// Use this for initialization
 	void Start () {
-	
+		xOffset = transform.position.x - balloon.position.x;
+		yOffset = transform.position.y - balloon.position.y;
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
-	
+		yClamp = Mathf.Clamp(balloon.position.y + yOffset,bottomLimit,1000f);
+		transform.position = new Vector3(balloon.position.x + xOffset, yClamp , -10f);
 	}
 }
 
