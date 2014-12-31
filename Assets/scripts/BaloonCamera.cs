1c6b34b590f3018931a54783214a2365153c217c
frankrs <frankrs3d@gmail.com>
2014-12-30 19:57:01 -0500

    made changes to frankscene started cam script

---
+++ Assets/scripts/BaloonCamera.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaloonCamera : MonoBehaviour {
+
+	public Transform balloon;
+
+	public float bottomLimit;
+
+	public float xOffset;
+
+	public float yOffset;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+	
+	}
+}
+
+
