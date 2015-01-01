6f4159f44db894aa0973aa4e99c4a0fd07c7f420
frankrs <frankrs3d@gmail.com>
2014-12-31 21:31:49 -0500

    added meters

--- Assets/scripts/BaloonMotor.cs
+++ Assets/scripts/BaloonMotor.cs
@@ -7,13 +7,25 @@ public class BaloonMotor : MonoBehaviour {
 	public Motor motor;
 
 
+	private float startDist;
+
+
+	void Start(){
+		startDist = transform.position.x;
+	}
+
 	void Update () {
 		// set static FlightInfo
 		FlightInfo.altitude = transform.position.y;
+		FlightInfo.distanceTraveled = transform.position.x - startDist;
+
 	}
 	
 
 	void FixedUpdate () {
+
+		FlightInfo.speed = transform.rigidbody2D.velocity.x;
+
 		if(controls.gasOn){
 			rigidbody2D.AddForce(new Vector2(0f,motor.gasPower));
 		}
@@ -33,4 +45,6 @@ public class Motor{
 
 public static class FlightInfo{
 	public static float altitude;
+	public static float distanceTraveled;
+	public static float speed;
 }
