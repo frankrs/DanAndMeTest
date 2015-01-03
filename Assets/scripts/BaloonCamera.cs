8f6fc60ea91a4b02ff99e53ca2b16e554e4c0d23
frankrs <frankrs3d@gmail.com>
2015-01-03 14:03:48 -0500

    mofified the camera to move to the right with speed

--- Assets/scripts/BaloonCamera.cs
+++ Assets/scripts/BaloonCamera.cs
@@ -10,6 +10,8 @@ public class BaloonCamera : MonoBehaviour {
 
 	public float startSize;
 	public float sizeLimit;
+	public float rightShiftComp;
+	public float rightShiftCompMax;
 
 	private float yOffset;
 	private float xOffset;
@@ -28,9 +30,14 @@ public class BaloonCamera : MonoBehaviour {
 	
 	// Update is called once per frame
 	void FixedUpdate () {
+
+		// get how far to shift the cam to the right to compensate for speed when zooming out
+		rightShiftComp = Mathf.Lerp(0f,rightShiftCompMax,FlightInfo.speed/100);
+
+		// clamp y so that he will land on the bottom
 		yClamp = Mathf.Clamp(balloon.position.y + yOffset,bottomLimit,1000f);
 		//transform.position = new Vector3(balloon.position.x + xOffset, yClamp , -10f);
-		transform.position = Vector3.Lerp(transform.position,new Vector3(balloon.position.x + xOffset, yClamp , -10f),lazy);
+		transform.position = Vector3.Lerp(transform.position,new Vector3(balloon.position.x + xOffset + rightShiftComp, yClamp , -10f),lazy);
 
 		camera.orthographicSize = Mathf.Lerp(startSize,sizeLimit,FlightInfo.speed/100);
 	}
