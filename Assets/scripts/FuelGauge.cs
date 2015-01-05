ce07cdbc8eab253d4cf563802c225309e87d220d
frankrs <frankrs3d@gmail.com>
2015-01-05 14:22:09 -0500

    added fuel gauge

--- Assets/scripts/FuelGauge.cs
+++ Assets/scripts/FuelGauge.cs
@@ -4,15 +4,17 @@ using UnityEngine.UI;
 
 public class FuelGauge : MonoBehaviour {
 
-	public Text guage;
+	public Image guage;
+	public float startW;
 
 	// Use this for initialization
 	void Start () {
-	
+		startW = guage.rectTransform.rect.width;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		guage.text = Mathf.Round(FlightInfo.fuel).ToString();
+		//guage.text = Mathf.Round(FlightInfo.fuel).ToString();
+		guage.rectTransform.sizeDelta = new Vector2(Mathf.Lerp(0f,startW,FlightInfo.fuel/100),guage.rectTransform.rect.height);
 	}
 }
